0766087ab9db630ad7c7f6096ef40b99afac45d2
Kevin Fraumeni <kevinup1010@gmail.com>
2024-05-06 11:20:10 -0400

    fixed syntax error: '__all__' list in the '__init__.py' file located in the 'Numberful' directory

--- Numberful/__init__.py
+++ Numberful/__init__.py
@@ -3,4 +3,4 @@ from .vector import Vector, Vec2, Vec3
 from .trigonometry import quadrant_id
 from .geometry import Calculate, Angle, Point2D, Line2D, Shape, Rectangle, Polygon, Circle
 
-__all__ = ["Vector", "Vec2", "Vec3", "quadrant_id, Calculate, Angle, Point2D, Line2D, Shape, Rectangle, Polygon, Circle"]
+__all__ = ["Vector", "Vec2", "Vec3", "quadrant_id, Calculate", "Angle, Point2D", "Line2D", "Shape", "Rectangle", "Polygon", "Circle"]
